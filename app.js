2f2c6ba033b1758c0e8692e7edd194d4a1b580c3
Nirvik <nirvik.agarwal@gmail.com>
2020-06-14 21:38:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var video = document.querySelector(".video");
+var slider = document.querySelector(".slider-color");
+var btn = document.getElementById("play-pause");
+
+function togglePlayPause() {
+    if (video.paused) {
+        btn.className = 'play';
+        video.play();
+    } else {
+        btn.className = "pause";
+        video.pause();
+    }
+}
+
+btn.onclick = function () { 
+    togglePlayPause();
+}
+
+video.addEventListener('timeupdate', function () {
+    var sliderPosition = video.currentTime / video.duration;
+    slider.style.width  = sliderPosition * 100 + "%";
+    if (video.ended) {
+        btn.className = "play";
+    }
+})
